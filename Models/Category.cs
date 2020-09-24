3360235d2e9d2875e27c15e021bd6ca71a396087
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-24 14:11:27 -0300

    changing model folders's name

--- Models/Category.cs
+++ Models/Category.cs
@@ -10,7 +10,7 @@ namespace APICsharp.Models
 
         [Required(ErrorMessage="Esse é um campo obrigatório")]
         [MaxLength(60,ErrorMessage="Este campo deve conter entre 3 e 60 caracteres")]
-        [MinLength(3,ErrorMessage="Este campo deve conter entre 3 e 60 caracteres")]
+         [MinLength(3,ErrorMessage="Este campo deve conter entre 3 e 60 caracteres")]
 
         public string Title {get; set;}
     }
